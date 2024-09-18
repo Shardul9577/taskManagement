e396541db47b7a707f5a6ef6e52b351d6953a57d
Shardul9577 <138381735+Shardul9577@users.noreply.github.com>
2024-09-18 20:32:13 +0530

    Delete backend directory

--- backend/db/db.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose')
-
-mongoose.connect("mongodb://localhost:27017/taskmanager")
-.then(()=>{
-    console.log(`The database is connected successfully.`)
-})
-.catch((err)=>{
-    console.log(`There is a error in this code : ${err}`)
-})
